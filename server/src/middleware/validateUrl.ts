42a85a39c3c519ab0f0a4ce97f068c79553d92fd
eraykeskinmac <eraykeskinmac@gmail.com>
2025-04-21 05:35:23 +0300

    Enhance YouTube URL validation in middleware and update routes
    - Renamed validateYoutube to validateYoutubeUrl for clarity.
    - Added comprehensive URL validation using the validator library.
    - Implemented regex check to ensure the URL is a valid YouTube link.
    - Integrated validation middleware into the video route for video info retrieval.

--- server/src/middleware/validateUrl.ts
+++ server/src/middleware/validateUrl.ts
@@ -1,15 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../utils/error";
 import { StatusCodes } from "http-status-codes";
+import validator from "validator";
 
-export const validateYoutube = (
+export const validateYoutubeUrl = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const [url] = req.body;
+  const { url } = req.body;
 
   if (!url) {
     return next(new AppError(StatusCodes.BAD_REQUEST, "URL is required"));
   }
+
+  if (!validator.isURL(url)) {
+    return next(new AppError(StatusCodes.BAD_REQUEST, "Invalid URL"));
+  }
+
+  // check if url is youtube url
+  const youtubeUrlRegex =
+    /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/;
+
+  if (!youtubeUrlRegex.test(url)) {
+    return next(
+      new AppError(StatusCodes.BAD_REQUEST, "URL is not a valid YouTube URL")
+    );
+  }
+
+  next();
 };
--- server/src/routes/video.route.ts
+++ server/src/routes/video.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { VideoController } from "../controllers/video.controller";
+import { validateYoutubeUrl } from "../middleware/validateUrl";
 
 const router = Router();
 
-router.post("/info", VideoController.getVideoInfo);
+router.post("/info", validateYoutubeUrl, VideoController.getVideoInfo);
 
 export default router;
