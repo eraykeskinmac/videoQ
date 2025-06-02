6a5fd8c05bebe6d2ec5772f173d8f1789ed5be6a
eraykeskinmac <eraykeskinmac@gmail.com>
2025-06-02 05:43:11 +0300

    Enhance Video entity with relationships to Transcription and Analysis
    - Added `transcription` and `analysis` fields to the `Video` entity, establishing one-to-one relationships with the `Transcription` and `Analysis` entities.
    - This update enables comprehensive management of video-related data, linking transcriptions and analysis directly to their respective videos.

--- server/src/entities/video.entity.ts
+++ server/src/entities/video.entity.ts
@@ -3,10 +3,13 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { User } from "./user.entity";
+import { Transcription } from "./transcription.entity";
+import { Analysis } from "./analysis.entity";
 
 @Entity()
 export class Video {
@@ -36,6 +39,12 @@ export class Video {
 
   @ManyToOne(() => User, (user) => user.videos, { nullable: false })
   user: User;
+
+  @OneToOne(() => Transcription, (transcription) => transcription.video)
+  transcription: Transcription;
+
+  @OneToOne(() => Analysis, (analysis) => analysis.video)
+  analysis: Analysis;
 
   @CreateDateColumn()
   createdAt: Date;
